8498a9abfd67227c15b3f11d331fffe9534b04b6
Daniel Guterding <daniel.guterding@gmail.com>
2023-12-22 10:12:37 +0100

    Auto format.

--- scripts/elk/convert.py
+++ scripts/elk/convert.py
@@ -2,8 +2,10 @@ import sys
 from math import ceil
 import numpy as np
 
+
 def get_num_kpoints(lines):
     return [int(n) for n in lines[7].strip().split()]
+
 
 def get_kvecs_and_remove_2Pi(lines):
     kvecs = []
@@ -11,13 +13,16 @@ def get_kvecs_and_remove_2Pi(lines):
         kvecs.append([float(k) * 0.5 / np.pi for k in l.strip().split()])
     return kvecs
 
+
 def write_header(fin, fout, lines, nx, ny, nz, kvecs):
     fout.writelines(lines[:7])
     fout.write(' {} {} {}\n'.format(nx - 1, ny - 1, nz - 1))
     fout.writelines(lines[8:9])
     for kx, ky, kz in kvecs:
-        fout.write('     {0: 1.8f}     {1: 1.8f}     {2: 1.8f}\n'.format(kx, ky, kz))
+        fout.write(
+            '     {0: 1.8f}     {1: 1.8f}     {2: 1.8f}\n'.format(kx, ky, kz))
     fout.writelines(lines[12:13])
+
 
 def get_energies_and_convert(lines, nx, ny, nz, startidx=13):
     energies = []
@@ -28,20 +33,24 @@ def get_energies_and_convert(lines, nx, ny, nz, startidx=13):
             break
     energies = np.array(energies)
     energies.resize((nx - 1, ny - 1, nz - 1))
-    energies *= 2 #Hartree to Rydberg
+    energies *= 2  # Hartree to Rydberg
     energies = energies.flatten()
     return energies, i + 1
+
 
 def write_energies(fout, energies, entries_per_line=6):
     num_lines = ceil(len(energies) / entries_per_line)
     for i in range(num_lines):
         dnidx = i * entries_per_line
         upidx = min(len(energies), dnidx + entries_per_line)
-        elstr = ' ' + ' '.join(['{0: 1.6e}'.format(e) for e in energies[dnidx:upidx]])
+        elstr = ' ' + ' '.join(['{0: 1.6e}'.format(e)
+                               for e in energies[dnidx:upidx]])
         fout.write(elstr + '\n')
+
 
 def write_footer(fout, lines, endidx):
     fout.writelines(lines[endidx:])
+
 
 if __name__ == '__main__':
     if not 3 == len(sys.argv):
